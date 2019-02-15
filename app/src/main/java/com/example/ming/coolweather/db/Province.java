7acd9e7806a2072cde899aad03b3c23ace2aee42
Ming <1070660980@qq.com>
2019-02-15 11:18:27 +0800

    加入创建数据库和表的各种配置。

---
+++ app/src/main/java/com/example/ming/coolweather/db/Province.java
@@ -0,0 +1,36 @@
+package com.example.ming.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+
+    private int id;
+
+    private String privinceName;
+
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPrivinceName() {
+        return privinceName;
+    }
+
+    public void setPrivinceName(String privinceName) {
+        this.privinceName = privinceName;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
